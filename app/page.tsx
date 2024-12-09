c7d0405a5a3386c3f24e320db2a9a6a395566a4f
ceejay077 <117851139+ceejay077@users.noreply.github.com>
2024-12-09 21:16:37 +0530

    upto services section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Banner from "./components/banner";
 import Header from "./components/header";
 import Hero from "./components/hero";
+import Logos from "./components/logos";
+import Services from "./components/services";
 
 export default function Home() {
   return (
@@ -8,6 +10,8 @@ export default function Home() {
       <Banner />
       <Header />
       <Hero />
+      <Logos />
+      <Services />
     </div>
   );
 }
