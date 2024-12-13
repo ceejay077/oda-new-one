a135b052cc596e8dc8fe40936474ab363adce5d3
ceejay077 <117851139+ceejay077@users.noreply.github.com>
2024-12-13 14:18:56 +0530

    margin-padding-fix

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -4,11 +4,11 @@ import Figures from "./sub-components/aboutFigurs";
 export default function about() {
   return (
     <div>
-      <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 pt-6 sm:py-12  ">
-        <div className="w-full items-center mx-auto max-w-screen-lg ">
-          <div className="group grid w-full grid-cols-2">
+      <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 pt-6 sm:py-12  min-w-full">
+        <div className="w-full items-center  max-w-screen-lg max-w-screen  min-w-full">
+          <div className="group grid  grid-cols-2">
             <div>
-              <div className="pr-12 mt-12">
+              <div style={{ marginTop: "178px", marginLeft: "200px" }}>
                 <h2 className="text-4xl text-gray-900 mb-9">About ODA</h2>
                 <p className="peer mb-6 text-gray-400">
                   One Direction Australia (ODA) is a leading commercial cleaning
@@ -26,9 +26,9 @@ export default function about() {
                             viewBox="0 0 20 20"
                           >
                             <path
-                              fill-rule="evenodd"
+                              fillRule="evenodd"
                               d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                              clip-rule="evenodd"
+                              clipRule="evenodd"
                             ></path>
                           </svg>
                         </div>
@@ -44,9 +44,9 @@ export default function about() {
                             viewBox="0 0 20 20"
                           >
                             <path
-                              fill-rule="evenodd"
+                              fillRule="evenodd"
                               d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                              clip-rule="evenodd"
+                              clipRule="evenodd"
                             ></path>
                           </svg>
                         </div>
@@ -62,9 +62,9 @@ export default function about() {
                             viewBox="0 0 20 20"
                           >
                             <path
-                              fill-rule="evenodd"
+                              fillRule="evenodd"
                               d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                              clip-rule="evenodd"
+                              clipRule="evenodd"
                             ></path>
                           </svg>
                         </div>
@@ -80,9 +80,9 @@ export default function about() {
                             viewBox="0 0 20 20"
                           >
                             <path
-                              fill-rule="evenodd"
+                              fillRule="evenodd"
                               d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                              clip-rule="evenodd"
+                              clipRule="evenodd"
                             ></path>
                           </svg>
                         </div>
@@ -99,9 +99,9 @@ export default function about() {
                             viewBox="0 0 20 20"
                           >
                             <path
-                              fill-rule="evenodd"
+                              fillRule="evenodd"
                               d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                              clip-rule="evenodd"
+                              clipRule="evenodd"
                             ></path>
                           </svg>
                         </div>
@@ -115,49 +115,17 @@ export default function about() {
 
                 {/* ------------------------------------------------------------------ */}
 
-                <div className="mt-4 text-gray-600">huks</div>
+                <div className=" text-white flex flex-row">
+                  <Figures />
+                  <Figures />
+                </div>
               </div>
             </div>
 
-            {/* <div className="pr-16 relative flex flex-col before:block before:absolute before:h-1/6 before:w-4 before:bg-blue-500 before:bottom-0 before:right-0 before:rounded-lg  before:transition-all group-hover:before:bg-orange-300 overflow-hidden">
-              <div className="absolute top-0 right-0 bg-blue-500 w-4/6 px-12 py-14 flex flex-col justify-center rounded-xl group-hover:bg-sky-600 transition-all ">
-                <span className="block mb-10 font-bold group-hover:text-orange-300">
-                  HERE WE ARE
-                </span>
-                <h2 className="text-white font-bold text-3xl">
-                  What started as a tiny team mostly dedicated to Air Quality
-                  has grown.
-                </h2>
-              </div>
-              <a
-                className="font-bold text-sm flex mt-2 mb-8 items-center gap-2"
-                href=""
-              >
-                <span>MORE ABOUT US</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
-                  />
-                </svg>
-              </a>
-              <div className="rounded-xl overflow-hidden">
-                <img src="https://picsum.photos/800/800" alt="" />
-              </div>
-            </div> */}
-
             <div>
-              <div className="">
+              <div style={{ marginRight: "200px" }}>
                 <img
-                  className="max-w-[760] max-h-[960] md:w-full mx-auto"
+                  className="max-w-[760] max-h-[960] md:w-full w-full "
                   src="/images/homePage/about.png"
                   alt="can_help_banner"
                 />
--- app/components/sub-components/aboutFigurs.tsx
+++ app/components/sub-components/aboutFigurs.tsx
@@ -3,10 +3,53 @@ import React from "react";
 export default function Figures() {
   return (
     <div>
-      <div className="flex justify-between">
-        <div>01</div>
-        <div>02</div>
-        <div>03</div>
+      <div className="  flex items-center justify-center p-4">
+        <div className=" rounded-xl  transition-all duration-300 hover:shadow-indigo-500/50 dark:hover:shadow-blue-900/50">
+          <div className="flex space-x-2 items-center animate-fade-in">
+            <div className="w-full">
+              <div className="font-semibold text-center md:text-left text-2xl text-indigo-800 dark:text-blue-400 mb-4">
+                200+
+              </div>
+              <div className="flex space-x-4 items-center flex-col md:flex-row">
+                <div className="flex -space-x-4 overflow-hidden p-2">
+                  <img
+                    className="inline-block h-6 w-6 rounded-full ring-2 ring-indigo-800 dark:ring-blue-900 hover:scale-110 transform duration-300 z-10 hover:z-30"
+                    src="https://randomuser.me/api/portraits/men/51.jpg"
+                    alt="Trusted user"
+                  />
+                  <img
+                    className="inline-block h-6 w-6 rounded-full ring-2 ring-indigo-800 dark:ring-blue-900 hover:scale-110 transform duration-300 z-20 hover:z-30"
+                    src="https://randomuser.me/api/portraits/women/4.jpg"
+                    alt="Trusted user"
+                  />
+                  <img
+                    className="inline-block h-6 w-6 rounded-full ring-2 ring-indigo-800 dark:ring-blue-900 hover:scale-110 transform duration-300 z-30 hover:z-30"
+                    src="https://randomuser.me/api/portraits/men/34.jpg"
+                    alt="Trusted user"
+                  />
+                  <img
+                    className="inline-block h-6 w-6 rounded-full ring-2 ring-indigo-800 dark:ring-blue-900 hover:scale-110 transform duration-300 z-40 hover:z-30"
+                    src="https://randomuser.me/api/portraits/women/6.jpg"
+                    alt="Trusted user"
+                  />
+                  <img
+                    className="inline-block h-6 w-6 rounded-full ring-2 ring-indigo-800 dark:ring-blue-900 hover:scale-110 transform duration-300 z-50 hover:z-30"
+                    src="https://randomuser.me/api/portraits/men/9.jpg"
+                    alt="Trusted user"
+                  />
+                  <img
+                    className="inline-block h-6 w-6 rounded-full ring-2 ring-indigo-800 dark:ring-blue-900 hover:scale-110 transform duration-300 z-60 hover:z-30"
+                    src="https://randomuser.me/api/portraits/women/9.jpg"
+                    alt="Trusted user"
+                  />
+                </div>
+                <div className="text-indigo-900 dark:text-blue-300 font-medium text-lg mt-4 md:mt-0">
+                  <p>Satisfied Customers</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
