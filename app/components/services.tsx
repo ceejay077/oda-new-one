877eb3d0cee12600320c48867a20346fe5c82218
ceejay077 <117851139+ceejay077@users.noreply.github.com>
2024-12-13 15:04:34 +0530

    update

--- app/components/services.tsx
+++ app/components/services.tsx
@@ -2,19 +2,19 @@ import React from "react";
 
 export default function Services() {
   return (
-    <div className="bg-white text-black flex justify-between">
-      <div className="max-w-screen-xl mx-auto py-8 px-4 lg:py-16 lg:px-6 bg-white">
+    <div className="bg-white text-black flex justify-center max-h-screen">
+      <div className="max-w-screen mx-auto py-8 lg:py-16  bg-white">
         <div className="text-center mb-10">
           <h2 className="text-4xl tracking-tight  text-black">
             Trusted Cleaning Experts
           </h2>
         </div>
 
-        <div className="flex flex-col md:flex-row">
+        <div className="flex flex-col md:flex-row w-full">
           {/* can help image */}
-          <div className="mr-0 md:mr-8 mb-6 md:mb-0">
+          <div className=" md:mr-8 mb-6 md:mb-0">
             <img
-              className="max-w-96 md:w-full mx-auto"
+              className="max-w-[800] max-h-[960] md:w-full "
               src="/images/homePage/girl.png"
               alt="can_help_banner"
             />
@@ -22,7 +22,7 @@ export default function Services() {
           {/* end can help image */}
 
           <div className="flex-1 flex flex-col sm:flex-row flex-wrap -mb-4 -mx-2">
-            <div className="w-full sm:w-1/2 mb-4 px-2">
+            <div className="w-4/5 sm:w-1/2 mb-4 px-2">
               <div className="h-full py-4 px-6 border border-green-500 border-t-0 border-l-0 rounded-br-xl">
                 <h3 className="text-2xl font-bold mb-6">
                   Dynamic Personalization
